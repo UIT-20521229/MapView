fed1da65a00b4441f785465c0d75496fdf797565
UIT-20521229 <20521229@gm.uit.edu.vn>
2024-06-24 00:12:53 +0700

    update

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -43,7 +43,7 @@ const MapView = () => {
           const query = message.query;
           if (query) {
             const response = await fetch(
-              `http://171.247.47.0/nominatim/search?q=${query}&format=json`,
+              `http://171.247.1.173/nominatim/search?q=${query}&format=json`,
               {
                 method: "GET",
                 headers: { "Content-Type": "application/json" },
@@ -95,7 +95,6 @@ const MapView = () => {
 
   return (
     <>
-      {console.log("nnn", region)}
       <MapContainer
         center={[region.latitude, region.longitude]}
         zoom={18}
@@ -105,7 +104,7 @@ const MapView = () => {
         className="map"
       >
         <TileLayer
-          url="http://171.247.47.0/map/{z}/{x}/{y}.png"
+          url="http://171.247.1.173/map/{z}/{x}/{y}.png"
         />
         <Marker position={[region.latitude, region.longitude]} />
         <MapComponent region={region} />
